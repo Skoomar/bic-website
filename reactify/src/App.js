c3cd4d92cf2f3c26d2b02f2b9a9734016c92162f
Skoomar <umar_farooqi@hotmail.co.uk>
2020-09-15 17:52:28 +0100

    Start implementing React

---
+++ reactify/src/App.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      {/*<header className="App-header">*/}
+
+      {/*</header>*/}
+
+        <div className="header-logo">
+            <img style={{width:"12%", height:"auto"}} alt="Header Logo" src="header-logo.png" />
+        </div>
+
+        <div id="nav-bar">
+            <div className="nav-button">
+                <p>Hello</p>
+            </div>
+            <div className="nav-button">
+                <p>Bye</p>
+            </div>
+            <div className="nav-button">
+                <p>About Us</p>
+            </div>
+        </div>
+
+        <div className="top">
+            <img style={{width:"40%", height:"auto"}} alt="Logo" src="logo.png" />
+        </div>
+
+        <div id="site-message">
+            <h1>Thank you for visiting</h1>
+            <h2>Site currently under construction, please visit again soon</h2>
+            <p>Contact: info@bilboroughislamiccentre.org.uk<br/><br/>
+                <strong>Please donate generously</strong><br/><br/>
+                <u>Bank details:</u><br/>
+                Bilborough Islamic Centre Trust<br/>
+                Sort code: 51-70-06<br/>
+                Account No.: 80859356
+            </p>
+        </div>
+
+    </div>
+  );
+}
+
+export default App;
