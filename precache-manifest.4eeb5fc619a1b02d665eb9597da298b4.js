681fb5faba77b44bdcf8393b98a83c541f520686
Skoomar <umar_farooqi@hotmail.co.uk>
2020-09-23 21:02:02 +0100

    Updates

---
+++ precache-manifest.4eeb5fc619a1b02d665eb9597da298b4.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b58de2fd3caac50b8be81ad32f3ac54d",
+    "url": "/bic-website/index.html"
+  },
+  {
+    "revision": "d31604b062f08f59f32e",
+    "url": "/bic-website/static/css/main.1c983801.chunk.css"
+  },
+  {
+    "revision": "edaeab1c82e4e5a61f19",
+    "url": "/bic-website/static/js/2.8d9e8f04.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/bic-website/static/js/2.8d9e8f04.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d31604b062f08f59f32e",
+    "url": "/bic-website/static/js/main.4ac85f87.chunk.js"
+  },
+  {
+    "revision": "f3feaee92e2534c4000d",
+    "url": "/bic-website/static/js/runtime-main.21b5869c.js"
+  },
+  {
+    "revision": "bd2f834c8814f551d06f22532d71c09e",
+    "url": "/bic-website/static/media/blue-bg.bd2f834c.png"
+  },
+  {
+    "revision": "cf2a413069142eb7676c4b8e4274eb64",
+    "url": "/bic-website/static/media/fb_icon.cf2a4130.png"
+  },
+  {
+    "revision": "13e9e2dd2f57cb89571a58af9586dcb7",
+    "url": "/bic-website/static/media/header-logo.13e9e2dd.png"
+  },
+  {
+    "revision": "7939f7ae16e64ce1518e21ce126fd089",
+    "url": "/bic-website/static/media/logo.7939f7ae.png"
+  },
+  {
+    "revision": "e644876c93b65e64342c516d931567d7",
+    "url": "/bic-website/static/media/prayer-timetable.e644876c.jpeg"
+  }
+]);
