d8871ebab3ffbfb49066fb529de5ff9ae375784c
Skoomar <umar_farooqi@hotmail.co.uk>
2020-09-25 15:29:31 +0100

    Fix typos etc in Home text

--- src/App.js
+++ src/App.js
@@ -42,98 +42,74 @@ function App() {
                     <div className="translucent-background">
                         <div id="about-us">
                             <p>
-                                Assalam-wa-alaikum wa rahimtullah wa barakaatu hu<br/><br/>
+                                <strong>Assalam-wa-alaikum wa rahimtullah wa barakaatu hu</strong><br/><br/>
 
-                                We are a registered charity with Charity Commission since 2018.<br/><br/>
+                                We are a registered charity with the Charity Commission since 2018.<br/><br/>
 
-                                Trustees have been very actively looking to buy a place for masjid
+                                Trustees have been very actively looking to buy a place for the masjid
                                 for the last 2-3 years prior to 2018 but no suitable options were available
                                 until Bilborough Baptist Church was put on sale.<br/><br/>
 
                                 We faced very tough competition from other interested parties keen
                                 to buy the place, but Allah SWT favoured us due to which we are
-                                privileged
-                                today.<br/>
+                                privileged own it now.<br/>
 
-                                Funding arrangement of substantial amount to pay towards building
-                                and other expenses were the biggest challenges but with help of Allah
-                                SWT and generosity of few individual made it possible to buy the church.<br/><br/>
+                                Acquiring the substantial amounts required to pay towards building
+                                and other expenses was the biggest challenge but with help of Allah
+                                SWT and the generosity of a few individual it was possible to buy the church.<br/><br/>
 
-                                We completed the purchase on 14th Dec 2018 when our journey of
-                                silent struggle has arrived to its first stop with the help of Allah SWT
+                                We completed the purchase on the 14th Dec 2018 and our journey of
+                                silent struggle arrived to its first stop with the help of Allah SWT
                                 and also the monetary contributions from brothers/sisters who sacrificed
                                 their worldly needs and made contribution into this great
                                 cause.<br/>
 
                                 Sadly; humanity has forgotten the purpose of their creation today,
-                                their
-                                master and the path of prophets/messengers who were deputed to
-                                remind
-                                how to spend life in this dunyia and prepare for life of unforeseen
-                                era
-                                we are heading to. Let’s make a pledge today to comeback to
-                                forgotten
-                                path and follow the guidance of Quran, Sunnah and path of Sehabah
-                                Rizwan
-                                ullalh ha ajmaeen.<br/><br/>
+                                their master and the path of the prophets/messengers who were deputed to
+                                remind us how to spend life in this dunyia and prepare for life of the 
+                                unforeseen era we are heading to. Let’s make a pledge today to comeback to
+                                the forgotten path and follow the guidance of the Quran, Sunnah and the 
+                                path of Sehabah Rizwan ullalh ha ajmaeen.<br/><br/>
 
 
-                                Idea of inception of this masjid is to use the platform to lean and
+                                The concept of this masjid is to use the platform to learn and
                                 connect to Allah, correct ourselves and invite others which is a
-                                prime
-                                duty assigned to us as being follower of last and final messenger.
-                                Our
-                                success of this duniya and akhira is left in Deen.<br/>
+                                prime duty assigned to us, being followers of the last and final messenger.
+                                Our success of this Duniya and Akhira is left in the Deen.<br/>
 
-                                On one side we have number of challenges but on the other hand we
-                                have
-                                big opportunity to work for our present and future generations.<br/>
+                                On one side we have a number of challenges but on the other hand we
+                                have a big opportunity to work for our present and future generations.<br/>
 
                                 In order to achieve these targets; we need a platform and also a
                                 continuous fiqar for Deen according to the path of Quran, Sunnah of
-                                Prophet PUBH and golden life of Sehabah Rizwan ullah ha
-                                ajmaeen.<br/><br/>
+                                Prophet PUBH and golden life of Sehabah Rizwan ullah ha ajmaeen.<br/><br/>
 
-                                To proceed on these lines; we have following achievement and plans
-                                which
-                                we would like to share with you and request your ownership,
-                                patronage
-                                and support during all the stages:<br/>
+                                To proceed on these lines; we have the following achievement and plans
+                                which we would like to share with you and request your ownership,
+                                patronage, and support throughout each stage:<br/>
 
                                 <ul>
-                                    <li>Establish five-time congregation prayer, weekly Juma and Eid
-                                        prayers
-                                        already started under the platform of Makki Masjid
-                                        Alhamdulillah.
+                                    <li>Establish five-time congregational prayer, weekly Juma and Eid
+                                        prayers already started under the platform of Makki Masjid, Alhamdulillah.
                                     </li>
-                                    <li>Afternoon/Evening Madrasa for Tajweed/Hifz started since
-                                        middle of
-                                        this 2020.
+                                    <li>Afternoon/Evening Madrasa for Tajweed/Hifz running since
+                                        the middle of 2020.
                                     </li>
                                     <li>Infrastructure redevelopment based on current and future
-                                        needs of
-                                        community.
+                                        needs of the community.
                                     </li>
                                     <li>Dawah Centre – to host activities to spread word of Allah
-                                        SWT using
-                                        all possible traditional and new state-of-art methods
-                                        including internet
-                                        and social media sources under the guidance of Quran, Sunnah
-                                        as directed
-                                        by respected Ulemahs/scholars/elders.
+                                        SWT using all possible traditional and new state-of-the-art methods
+                                        including internet and social media sources under the guidance of Quran, Sunnah
+                                        as directed by respected Ulemahs/scholars/elders.
                                     </li>
                                     <li>Community gathering program and delivering sessions on
-                                        socio-economic
-                                        topics.
+                                        socio-economic topics.
                                     </li>
                                     <li>Walk-in centre for nonbelievers to provide them a glimpse of
-                                        Islam
-                                        basics (long-term).
+                                        the basics of Islam.
                                     </li>
-                                    <li>Janaza/Gusal/dead body storage and Janaza prayer facilities
-                                        (long
-                                        term)
-                                    </li>
+                                    <li>Janaza/Gusal/dead body storage and Janaza prayer facilities (long term)</li>
                                 </ul>
 
                                 Jazak Allah khairun<br/><br/>
