a80c4f74d3586afc014d9dd522a135bad402d0ec
Umar Farooqi <umar_farooqi@hotmail.co.uk>
2025-02-24 23:22:33 +0000

    Updates

---
+++ precache-manifest.c8333c664caaa0ed607ea69db191f80f.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "16192ba53bd6092f9781e710c63634a8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "69a8a7ac7b400f63f178",
+    "url": "/static/css/main.2f74d912.chunk.css"
+  },
+  {
+    "revision": "277638a28c0363d1b5c6",
+    "url": "/static/js/2.93aa1727.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.93aa1727.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "69a8a7ac7b400f63f178",
+    "url": "/static/js/main.81c502be.chunk.js"
+  },
+  {
+    "revision": "bab77f91a0c24a16bb0f",
+    "url": "/static/js/runtime-main.46b484da.js"
+  },
+  {
+    "revision": "bd2f834c8814f551d06f22532d71c09e",
+    "url": "/static/media/blue-bg.bd2f834c.png"
+  },
+  {
+    "revision": "13e9e2dd2f57cb89571a58af9586dcb7",
+    "url": "/static/media/header-logo.13e9e2dd.png"
+  },
+  {
+    "revision": "7939f7ae16e64ce1518e21ce126fd089",
+    "url": "/static/media/logo.7939f7ae.png"
+  },
+  {
+    "revision": "13f0485633e4ce8164c4e36ed9b90258",
+    "url": "/static/media/prayer-timetable.13f04856.jpeg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.a91231143a28260e2f88f0fbdd4958a6.js"
+  "/precache-manifest.c8333c664caaa0ed607ea69db191f80f.js"
 );
 
 self.addEventListener('message', (event) => {
